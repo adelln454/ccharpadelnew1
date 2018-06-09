63d05d14a49293ac4d472ebd6ed1fa619c975f2d
adelln454 <40113485+adelln454@users.noreply.github.com>
2018-06-09 15:18:40 +0300

    add  a comment

---
+++ ADEL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADEL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my farst program
+             
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
